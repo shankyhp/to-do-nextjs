3a9af5e01c401c926c28b9044dd18f782053ee66
shankyhp <birajdarhpshankar@gmail.com>
2025-01-01 20:55:33 +0530

    To-Do next js Completed

---
+++ app/api/route.js
@@ -0,0 +1,60 @@
+import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/TodoModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+
+LoadDB();
+
+//to fetch all notes
+
+export async function GET(request) {
+
+    const todos = await TodoModel.find({})
+    return NextResponse.json({ todos: todos })
+}
+
+//to create notes
+
+export async function POST(request) {
+
+
+    const { title, description } = await request.json();
+
+    await TodoModel.create({
+        title,
+        description
+    })
+
+    return NextResponse.json({ msg: "TODO Created" })
+}
+
+
+//delete notes
+export async function DELETE(request) {
+
+
+    const mongoId = await request.nextUrl.searchParams.get('mongoId');
+
+    await TodoModel.findByIdAndDelete(mongoId)
+
+    return NextResponse.json({ msg: "TODO Deleted" })
+}
+
+//change status
+
+export async function PUT(request) {
+
+
+    const mongoId = await request.nextUrl.searchParams.get('mongoId');
+
+    await TodoModel.findByIdAndUpdate(mongoId, {
+        $set: {
+            isCompleted: true
+        }
+    })
+
+    return NextResponse.json({ msg: "TODO Completed" })
+}
